0f22f43e5976bfa2df7d624d63b58848f88a1cdd
Daniel Boadzie <boadziedaniel43@gmail.com>
2020-07-21 09:13:54 +0000

    added a display of articles

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
 import React from "react";
+import Articles from "../components/Articles";
+import Query from "../components/Query";
+import ARTICLES_QUERY from "../apollo/queries/article/articles";
 
-function index() {
+const Home = () => {
   return (
     <div>
-      <h1>Blogman</h1>
+      <div className="uk-section">
+        <div className="uk-container uk-container-large">
+          <h1>Blogman</h1>
+          <Query query={ARTICLES_QUERY}>
+            {({ data: { articles } }) => {
+              return <Articles articles={articles} />;
+            }}
+          </Query>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default index;
+export default Home;
